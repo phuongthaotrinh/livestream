5f054503d0d7d75efcb8213e72dadfb3b6707179
dong <lodong601@gmail.com>
2023-12-24 20:13:26 +0700

    update news api

--- src/routes/additional.js
+++ src/routes/additional.js
@@ -4,4 +4,6 @@ const router = express.Router();
 const additional_controller = require('../controllers/AdditionalController.js');
 router.post('/add-bulk-slide',TokenCheckMiddleware,additional_controller.addSlide);
 router.get('/get-all-slide',TokenCheckMiddleware,additional_controller.getAllSlide);
+router.get('/get-all-news',TokenCheckMiddleware,additional_controller.getNews);
+router.post('/add-or-update-news',TokenCheckMiddleware,additional_controller.addNewsOrUpdate);
 module.exports = router;
